e20fa2e99c55de8121245b366cbd75491ba27a81
thais <thaisschneiderhepp@gmail.com>
2023-02-13 09:33:10 -0300

    comecando

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', function(req, res){
+    res.send("ola thais")
+
+})
 
 app.listen(3000, function () {
     console.log("conexão inicializada na porta 3000")
