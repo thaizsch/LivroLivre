20730a5f7fe69e7aa367726080d875a62bdf8c29
thais <thaisschneiderhepp@gmail.com>
2023-02-15 08:26:41 -0300

    banana

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+require ('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const app = express()
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
@@ -13,12 +17,45 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+///////////////////////////////////////////////ROTAS//////////////////////////////////////////////////
 
-app.get('/', function(req, res){
-    res.send("ola thais")
+                                              //get//
+                                            //public//
+app.get('/', (req, res) =>{
+    res.render('inicio.ejs', {})
 
 })
+                                        //pagina inicial//
+app.get('/paginainicial', function(req, res){
+    res.render('index.ejs', {})
 
-app.listen(3000, function () {
-    console.log("conexão inicializada na porta 3000")
 })
+                                        //perfil do usuario//
+app.get('/usuarios', function(req, res){
+    res.render('usuarios.ejs', {})
+
+})
+                                        //teste do post /add//
+//add
+app.get('/add', function(req, res){
+    res.render('adiciona.')
+})
+//post
+app.post('/add',function(req, res){
+    console.log("chegou aqui")
+})
+
+//banco de dados
+//const dbUser = process.env.DB_USER 
+//const dbPassword = process.env.DB_PASS
+
+//mongoose
+//    .connect()
+//   .then(() => {
+
+        app.listen(3000, function () {
+            console.log("conexão inicializada na porta 3000")
+        })
+//    })
+//    .catch((err) => console.log(err))
+
